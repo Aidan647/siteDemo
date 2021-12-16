cacf984189a8837b3059108a4c9b5161583a7a66
Aidan647 <kirillkota@gmail.com>
2021-12-16 23:20:14 +0200

    v2

---
+++ assets/main.js
@@ -0,0 +1,7 @@
+
+document.getElementById("contact-btn").addEventListener("mouseenter", () => {
+	document.getElementById("contact").classList.add("animate")
+})
+document.getElementById("contact-btn").addEventListener("mouseleave", () => {
+	document.getElementById("contact").classList.remove("animate")
+})
